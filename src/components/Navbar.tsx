2c05928346a87a505e988735d55ade58d1d56c31
Lorenna <lohluizesd@gmail.com>
2025-08-31 19:59:10 -0300

    feat: criar Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,29 +4,35 @@ import { Button } from './Button';
 interface NavbarProps {
   logo?: string;
 }
-
 export function Navbar({ logo = Logo }: NavbarProps) {
   return (
-    <nav>
+    <nav className="bg-red-200 flex justify-between items-center p-4">
       <a href="#">
-        <img src={logo} alt="Logo" />
+        <img src={logo} alt="Logo" className="h-[60px] w-[80px]" />
       </a>
-
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">What we sell</a>
-        </li>
-        <li>
-          <a href="#">About us</a>
-        </li>
-        <li>
-          <a href="#">Testimonials</a>
-        </li>
-      </ul>
-      <Button>Texto</Button>
+      <div className="flex item-center gap-8">
+        <ul className="flex gap-6 items-center">
+          <li>
+            <a
+              href="#"
+              className="hover:text-gray-300 transition-colors ml-[20px]"
+            >
+              Link 1
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-300 transition-colors">
+              Link 2
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-300 transition-colors">
+              Link 3
+            </a>
+          </li>
+        </ul>
+        <Button className="h-[40px] px-6 mt-2">Book a spot</Button>
+      </div>
     </nav>
   );
 }
